263f74ae3c547b3c568be3c0d704b3d019b089b4
QueenJammie <jammie.lacc@outlook.com>
2022-06-29 00:47:18 -0400

    Added a search engine and a few styling effect, nothing is up-to-date

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Dictionary from './Dictionary';
 
 import './App.css';
 
@@ -6,11 +7,19 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <div className='row mt-5'>
-            <h1 class="ms-2 mb-0"><img src={logo} className="App-logo" alt="logo" />React Dictionary app.</h1>
-          </div>
-        </header>
+        <body>
+          <header className="App-header">
+            <div className='row mt-5'>
+              <h1 class="ms-2 mb-0"><img src={logo} className="App-logo" alt="logo" />React Dictionary app.</h1>
+            </div>
+          </header>
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="ms-2">
+            <p><a href="https://github.com/QueenJammie/dictionary-app/tree/main/src" target="_blank" rel="noopener noreferrer">Open-source code</a> by <a href="https://www.shecodes.io/hall-of-fame?q=Jammie+Lachance-C%C3%B4t%C3%A9&commit=Search" target="_blank" rel="noopener noreferrer">Jammie Lachance-Côté</a></p>
+          </footer>
+        </body>
       </div>
     </div>
   );
---
+++ src/Dictionary.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+import "./Dictionary.css";
+
+export default function Dictionary()
+{
+  let [keyword, setKeyword] = useState("");
+
+  function updateKeywordChange(event)
+  {
+    setKeyword(event.target.value);
+  }
+
+  function search(event)
+  {
+    event.preventDefault();
+    alert(`Searching the definition for ${keyword}`);
+  }
+
+  return (
+    <div className="Dictionary mt-5 p-3 border">
+      <form onSubmit={search}>
+        <div className="row">
+          <div className="col-6 mb-3">
+            <input type="search" placeholder="What do you have in mind?" autofocus="on" className="form-control" onChange={updateKeywordChange} />
+          </div>
+          <div className="col-3 mb-2">
+            <input type="submit" value="Search" className="btn btn-success ms-2" />
+          </div>
+        </div>
+      </form>
+      
+      <div className="col">
+      <h3><span className="speakerIcon">🔉</span> Pronunciation</h3>
+        <div className="ms-3">
+          <h4>Apple</h4>
+          <ul>
+            <li>ap·ple</li>
+            <li className="phonetic">/'apəl/</li>
+          </ul>
+         
+        </div>
+      </div>
+      <h3>Noun</h3>
+      <ul className="nounUl">
+        <li>the round fruit of a tree of the rose family, which typically has thin red 
+        or green skin and crisp flesh. Many varieties have been developed 
+        as dessert or cooking fruit or for making cider.</li>
+        <li>the tree which bears apples.</li>
+      </ul>
+      <img src="https://i5.walmartimages.ca/images/Enlarge/094/514/6000200094514.jpg" alt="Walmart apple" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="Apple.inc" />
+      <img src="https://www.jardiner-malin.fr/wp-content/uploads/2021/09/pommier.jpg" alt="Pommier" />
+      <img src="http://4.bp.blogspot.com/-o6g58a6fRG0/U1Pxya3QbiI/AAAAAAAAJMw/FAvqau4WTpQ/s1600/P4205939.JPG" alt="Apple pie" />
+      
+      
+    </div>
+  )
+}
