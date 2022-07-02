ffda00b7a0f7ff45195f322a5f50bde849a09106
QueenJammie <jammie.lacc@outlook.com>
2022-07-02 14:23:03 -0400

    changed margin?

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
           <main>
             <Dictionary defaultKeyword="sunrise" />
           </main>
-          <footer className="footer ms-2">
+          <footer className="footer mt-2">
             <p><a href="https://github.com/QueenJammie/dictionary-app/tree/main/src" target="_blank" rel="noopener noreferrer"><strong>Open-source code</strong></a> by <a href="https://www.shecodes.io/hall-of-fame?q=Jammie+Lachance-C%C3%B4t%C3%A9&commit=Search" target="_blank" rel="noopener noreferrer"><strong>Jammie Lachance-Côté</strong></a></p>
           </footer>
         </body>
