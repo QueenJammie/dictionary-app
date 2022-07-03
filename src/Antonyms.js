7a7a27bea26fe805d495f56b21b01691589e15e9
QueenJammie <jammie.lacc@outlook.com>
2022-07-03 16:51:46 -0400

    Add a conditional rendering for one or many or null result

--- src/Antonyms.js
+++ src/Antonyms.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function Antonyms(props) {
-  if (props.antonyms && props.antonyms.length > 0) {
+  if (props.antonyms && props.antonyms.length === 1) {
+    return (
+      <div>
+        <strong>Antonym: </strong>
+        <ul className="Antonyms ps-3">
+          {props.antonyms.map(function (antonym, index) {
+            return <li key={index}>{antonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+  else if (props.antonyms && props.antonyms.length > 1) {
     return (
       <div>
         <strong>Antonyms: </strong>
@@ -12,7 +24,8 @@ export default function Antonyms(props) {
         </ul>
       </div>
     );
-  } else {
+  }
+  else {
     return null;
   }
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  if (props.synonyms && props.synonyms.length > 0) {
+  if (props.synonyms && props.synonyms.length === 1) {
+    return (
+      <div>
+        <strong>Synonym: </strong>
+        <ul className="Synonyms ps-3">
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+  if (props.synonyms && props.synonyms.length > 1) {
     return (
       <div>
         <strong>Synonyms: </strong>
@@ -13,7 +25,8 @@ export default function Synonyms(props) {
         </ul>
       </div>
     );
-  } else {
+  }
+  else {
     return null;
   }
 }
