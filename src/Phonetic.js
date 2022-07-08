088b560947448f04d7a081c9ab39fa43057ca683
QueenJammie <jammie.lacc@outlook.com>
2022-07-08 17:52:43 -0400

    Add a conditional rendering on the phonitic rending

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,6 +3,8 @@ import "./Phonetic.css";
 
 export default function Phonetic(props)
 {
+    if (props.phonetic.audio)
+    {
     return (
         <div className="Phonetic">
             <a href={props.phonetic.audio} target="_blank" rel="noreferrer" className="a-btn"><span className="speakerIcon">🔉</span>Listen to phonetic</a>
@@ -10,4 +12,9 @@ export default function Phonetic(props)
             <a href={props.phonetic.sourceUrl} target="_blank" rel="noreferrer" className="sourceUrl">{props.phonetic.sourceUrl}</a>
         </div>
     );
+    }
+    else
+    {
+        return null;
+    }
 }
